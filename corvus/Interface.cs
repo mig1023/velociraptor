61a54ab37ef6c45260f3e120e305c8a70932a736
mig1023 <mig1023@yandex.ru>
2019-06-01 22:28:35 +0300

    page interface prototype

---
+++ corvus/Interface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace corvus
+{
+    class Interface
+    {
+        public enum moveDirection { horizontal, vertical };
+
+        public static void FullScreen(MainWindow main)
+        {
+            main.WindowState = WindowState.Maximized;
+            main.WindowStyle = WindowStyle.None;
+
+            foreach (Canvas canvas in new List<Canvas> { main.white, main.red, main.blue })
+            {
+                canvas.Width = SystemParameters.PrimaryScreenWidth;
+                canvas.Height = SystemParameters.PrimaryScreenHeight;
+            }
+
+            main.red.Margin = new Thickness(SystemParameters.PrimaryScreenWidth + 5, 0, 0, 0);
+            main.blue.Margin = new Thickness(0, SystemParameters.PrimaryScreenHeight + 5, 0, 0);
+        }
+
+        public static void Move(Canvas moveCanvas, Canvas prevCanvas, moveDirection direction = moveDirection.horizontal)
+        {
+            double left = (direction == moveDirection.horizontal ? 0 : moveCanvas.Margin.Left);
+            double top = (direction == moveDirection.vertical ? 0 : moveCanvas.Margin.Top);
+
+            ThicknessAnimation move = new ThicknessAnimation();
+            move.Duration = TimeSpan.FromSeconds(0.2);
+            move.From = moveCanvas.Margin;
+
+            move.To = new Thickness(left, top, moveCanvas.Margin.Right, moveCanvas.Margin.Bottom);
+
+            moveCanvas.BeginAnimation(FrameworkElement.MarginProperty, move);
+
+            left = (direction == moveDirection.horizontal ?
+                prevCanvas.Margin.Left - moveCanvas.Margin.Left : prevCanvas.Margin.Left);
+            top = (direction == moveDirection.vertical ?
+                prevCanvas.Margin.Top - moveCanvas.Margin.Top : prevCanvas.Margin.Top);
+
+            move.From = prevCanvas.Margin;
+
+            move.To = new Thickness(left, top, prevCanvas.Margin.Right, prevCanvas.Margin.Bottom);
+
+            prevCanvas.BeginAnimation(FrameworkElement.MarginProperty, move);
+        }
+    }
+}
---
+++ corvus/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace corvus
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Interface.FullScreen(this);
+        }
+
+        private void toWhite_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.Move(
+                moveCanvas: white,
+                prevCanvas: red
+            );
+        }
+
+        private void toRed_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.Move(
+                moveCanvas: red,
+                prevCanvas: white
+            );
+        }
+
+        private void toWhiteFromBlue_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.Move(
+                moveCanvas: white,
+                prevCanvas: blue,
+                direction: Interface.moveDirection.vertical
+            );
+        }
+
+        private void toBlue_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.Move(
+                moveCanvas: blue,
+                prevCanvas: white,
+                direction: Interface.moveDirection.vertical
+            );
+        }
+    }
+}
