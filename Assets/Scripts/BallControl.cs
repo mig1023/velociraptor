9d73364e02103fc0bc0c952b3ac808736c519b24
mig1023 <mig1023@mail.ru>
2023-05-27 15:26:55 +0300

    ball control ltl refactoring

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -8,26 +8,14 @@ public class BallControl : MonoBehaviour
     {
         Launch();
     }
-
-    void Update()
-    {
-
-    }
 	
 	public void Launch()
     {
-        float random = Random.Range(0f, 1f);
-
         GameObject.Find("Ball").GetComponent<Rigidbody2D>().transform.position = new Vector2(0, 0);
         GameObject.Find("Ball").GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
-        if (random <= 0.5f)
-        {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(-250f, 120f * (0.5f - random)));
-        }
-        else
-        {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(250f, 120f * (0.5f - random)));
-        }
+		float random = Random.Range(0f, 1f);
+		float randomDirection = random <= 0.5f ? -250f : 250f;
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(randomDirection, 120f * (0.5f - random)));
     }
 }
