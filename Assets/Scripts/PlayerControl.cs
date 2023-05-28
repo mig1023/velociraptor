5e71814f330e3a6b5f8659d09787b6a347cbdf18
mig1023 <mig1023@mail.ru>
2023-05-28 14:35:30 +0300

    enemy moving inhibitor

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,16 +8,24 @@ public class PlayerControl : MonoBehaviour
 	public KeyCode moveDown;
 
 	public float speed = 10;
+	float inhibitor;
 	
 	void Update()
 	{
 		if (moveUp == KeyCode.None)
 		{
+			inhibitor -= Time.deltaTime;
+			
+			if (inhibitor > 0)
+				return;
+			
 			float ballPos = GameObject.Find("Ball").GetComponent<Rigidbody2D>().transform.position.y;
 			float playerPos = GetComponent<Rigidbody2D>().transform.position.y;
 			float move = ballPos > playerPos ? 5 : -5;
 
 			GetComponent<Rigidbody2D>().velocity = new Vector2(0, move);
+			
+			inhibitor = 0.2f;
 		}
 		else
 		{
