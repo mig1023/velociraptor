388843ae11793842808abd9288cee002caf9597e
mig1023 <mig1023@mail.ru>
2023-06-01 18:09:55 +0300

    borders fixes

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -48,10 +48,10 @@ public class Level : MonoBehaviour
 		bottom.offset = new Vector2(0f, -1 * camera.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y - 0.5f);
 
 		left.size = new Vector2(1f, camera.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 0f)).y);
-		left.offset = new Vector2(-1 * camera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x - 0.5f, 0f);
+		left.offset = new Vector2(-1 * camera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x - 2.5f, 0f);
 
 		right.size = new Vector2(1f, camera.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y);
-		right.offset = new Vector2(camera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
+		right.offset = new Vector2(camera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 2.5f, 0f);
 	}
 	
 	void OnGUI()
