faa79a9edf9467b33365e667f3210a131fd64cd2
mig1023 <mig1023@mail.ru>
2023-06-03 15:47:22 +0300

    'problems' moving rm

--- Assets/Scripts/Problems.cs
+++ Assets/Scripts/Problems.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Problems : MonoBehaviour
 {
 	private float rotate = 0f;
-	private float move = 0f;
 		
 	void Update()
 	{
@@ -14,27 +13,10 @@ public class Problems : MonoBehaviour
 			rotate -= 0.05f;
 			transform.Rotate(0f, 0f, 1f);
 		}
-		else if (move > 0f)
-		{
-			move -= 0.01f;
-			float moving = 0;
-			
-			if (transform.position.y < -5.3f)
-				moving = 0.05f;
-			else if ((transform.position.y < 0f) && (transform.position.y > -4.8f))
-				moving = -0.05f;
-			else if ((transform.position.y > 0f) && (transform.position.y < 4.8f))
-				moving = 0.05f;
-			else if (transform.position.y > 5.3f)
-				moving = -0.05f;
-			
-			transform.position = new Vector2(transform.position.x, transform.position.y + moving);
-		}
 	}
 	
 	public void OnCollisionEnter2D(Collision2D collision)
 	{
 		rotate += Random.Range(5f, 25f);
-		move += Random.Range(1f, 5f);
 	}
 }
