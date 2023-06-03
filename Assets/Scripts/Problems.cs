300d441d414fbbc7903b9c9a8c284f5d40b46c71
mig1023 <mig1023@mail.ru>
2023-06-03 15:21:55 +0300

    'problems' moving

--- Assets/Scripts/Problems.cs
+++ Assets/Scripts/Problems.cs
@@ -11,16 +11,30 @@ public class Problems : MonoBehaviour
 	{
 		if (rotate > 0f)
 		{
-			Debug.Log("NEED rotate");
-			Debug.Log("rotate:" + rotate);
-			
 			rotate -= 0.05f;
 			transform.Rotate(0f, 0f, 1f);
+		}
+		else if (move > 0f)
+		{
+			move -= 0.01f;
+			float moving = 0;
+			
+			if (transform.position.y < -5.3f)
+				moving = 0.05f;
+			else if ((transform.position.y < 0f) && (transform.position.y > -4.8f))
+				moving = -0.05f;
+			else if ((transform.position.y > 0f) && (transform.position.y < 4.8f))
+				moving = 0.05f;
+			else if (transform.position.y > 5.3f)
+				moving = -0.05f;
+			
+			transform.position = new Vector2(transform.position.x, transform.position.y + moving);
 		}
 	}
 	
 	public void OnCollisionEnter2D(Collision2D collision)
 	{
 		rotate += Random.Range(5f, 25f);
+		move += Random.Range(1f, 5f);
 	}
 }
