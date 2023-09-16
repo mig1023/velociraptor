4622d679493998c5fbcbf3194b6c4044b4e30b38
mig1023 <mig1023@mail.ru>
2023-09-16 13:47:16 +0300

    edit link + view fixes

--- velociraptor/Pages/View.cshtml.cs
+++ velociraptor/Pages/View.cshtml.cs
@@ -7,12 +7,14 @@ namespace velociraptor.Pages
     public class ViewModel : PageModel
     {
         public Article Article { get; set; }
+        public string EditLink { get; set; }
 
         public IActionResult OnGet(string title)
         {
             if (ORM.Db.Exists(title, out Article article))
             {
                 Article = article;
+                EditLink = Url.Page("Edit", new { title = title });
                 return Page();
             }
             else
