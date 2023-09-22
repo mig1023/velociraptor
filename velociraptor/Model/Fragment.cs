640b3152fd040433c2a617b8bda46fb94b52e0af
mig1023 <mig1023@mail.ru>
2023-09-22 16:17:28 +0300

    Fragment class

---
+++ velociraptor/Model/Fragment.cs
@@ -0,0 +1,13 @@
+﻿using static velociraptor.Model.History;
+
+namespace velociraptor.Model
+{
+    public class Fragment
+    {
+        public string Text { get; set; }
+
+        public ChangeType Type { get; set; }
+
+        public bool NoNewLine { get; set; }
+    }
+}
