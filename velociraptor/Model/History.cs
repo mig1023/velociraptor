82713d2a9a8b50c9946ba2bbb24dfb6e1070f3b2
mig1023 <mig1023@mail.ru>
2023-09-22 16:15:16 +0300

    History class

---
+++ velociraptor/Model/History.cs
@@ -0,0 +1,18 @@
+﻿namespace velociraptor.Model
+{
+    public class History
+    {
+        public enum ChangeType
+        {
+            Nope,
+            Deleted,
+            Inserted
+        }
+
+        public string Text { get; set; }
+
+        public int Pos { get; set; }
+
+        public ChangeType Type { get; set; }
+    }
+}
