47ff5479007e3b1e46b35d82236d60549d36b5b4
mig1023 <mig1023@mail.ru>
2023-09-22 16:19:33 +0300

    Fragment's Get method

--- velociraptor/Model/Fragment.cs
+++ velociraptor/Model/Fragment.cs
@@ -9,5 +9,31 @@ namespace velociraptor.Model
         public ChangeType Type { get; set; }
 
         public bool NoNewLine { get; set; }
+
+        static List<Fragment> Get(string text, ChangeType type)
+        {
+            List<Fragment> fragments = new List<Fragment>();
+
+            if (String.IsNullOrEmpty(text))
+                return fragments;
+
+            List<String> lines = text.Split("\n").ToList();
+
+            foreach (string line in lines)
+            {
+                Fragment fragment = new Fragment
+                {
+                    Text = line,
+                    Type = type,
+                };
+
+                fragments.Add(fragment);
+            }
+
+            if (!text.EndsWith("\n") && fragments.Count > 0)
+                fragments[fragments.Count - 1].NoNewLine = true;
+
+            return fragments;
+        }
     }
 }
