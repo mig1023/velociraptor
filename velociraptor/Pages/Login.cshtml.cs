bca55b5de0d637f368c911d596d2bfb5f45fa035
mig1023 <mig1023@mail.ru>
2023-10-28 11:52:12 +0300

    Identity in Layout

--- velociraptor/Pages/Login.cshtml.cs
+++ velociraptor/Pages/Login.cshtml.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 using velociraptor.ORM;
 
 namespace velociraptor.Pages
